32b882325c28ebb6530bce7f69d463ec3370a62d
meganekkogekirabu <saphgalipault@gmail.com>
2025-04-03 11:51:17 -0400

    only iterate over pages which link to Babel

--- disable babel cat.py
+++ disable babel cat.py
@@ -4,7 +4,8 @@ from datetime import timedelta
 from pywikibot.pagegenerators import AllpagesPageGenerator
 
 site = pywikibot.Site()
-gen = AllpagesPageGenerator(namespace=2, site=site)
+tl_page = pywikibot.Page(site, "Template:Babel")
+gen = pywikibot.page.BasePage(tl_page).getReferences(only_template_inclusion=True, namespaces=2)
 
 for page in gen:
     title = page.title()
@@ -16,12 +17,12 @@ for page in gen:
             delta = (site.server_time() - last_edit[2]) >= timedelta(days=730)
             
             if delta and "Babel" in page.text and "inactive" not in page.text:
-                page.text = re.sub(r"({{Babel[^}]+)", r"\1|inactive=1", page.text)
-                page.save("mark users whose last contribution was more than 2 years ago as inactive in bABEL")
+                #page.text = re.sub(r"({{Babel[^}]+)", r"\1|inactive=1", page.text)
+                #page.save("mark users whose last contribution was more than 2 years ago as inactive in Babel")
                 
             elif not delta and "inactive" in page.text:
-                page.text = re.sub(r"({{Babel[^}]+)\|inactive=1", r"\1")
-                page.save("enable Babel categorisation, which was disabled despite last edit being recent")
+                #page.text = re.sub(r"({{Babel[^}]+)\|inactive=1", r"\1")
+                #page.save("enable Babel categorisation, which was disabled despite last edit being recent")
         
         except:
             # Probably a redirect from a rename; just skip.
