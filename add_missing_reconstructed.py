bf675129166d715f4d1430439911430511c70fd5
Madeleine <gankiann.niu@gmail.com>
2025-09-11 21:32:12 -0400

    +script

---
+++ add_missing_reconstructed.py
@@ -0,0 +1,10 @@
+import pywikibot
+from pywikibot import pagegenerators
+
+site = pywikibot.Site()
+cat = pywikibot.Category(site, "Category:Entries missing Template:reconstructed by language")
+gen = pagegenerators.CategorizedPageGenerator(cat, namespaces=118)
+
+for page in pagegenerators.PreloadingGenerator(gen, 10):
+    page.text = "{{reconstructed}}\n" + page.text
+    page.save("add {{reconstructed}}")
