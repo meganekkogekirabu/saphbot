d080010da69488c1c7d775c083c69220484fd265
Madeleine <gankiann.niu@gmail.com>
2025-09-20 11:54:34 -0400

    needs to be recursive

--- add_missing_reconstructed.py
+++ add_missing_reconstructed.py
@@ -3,7 +3,7 @@ from pywikibot import pagegenerators
 
 site = pywikibot.Site()
 cat = pywikibot.Category(site, "Category:Entries missing Template:reconstructed by language")
-gen = pagegenerators.CategorizedPageGenerator(cat, namespaces=118)
+gen = pagegenerators.CategorizedPageGenerator(cat, recurse=True, namespaces=118)
 
 for page in pagegenerators.PreloadingGenerator(gen, 10):
     page.text = "{{reconstructed}}\n" + page.text
