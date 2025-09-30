9c9845883c58dc33e6a3022581e54002922c81ca
Madeleine <gankiann.niu@gmail.com>
2025-09-29 23:03:41 -0400

    add count option to dump_grep

--- lib/dump_grep.py
+++ lib/dump_grep.py
@@ -1,17 +1,10 @@
 from argparse import ArgumentParser
 from lxml import etree
 import re
-import signal
 import sys
 from tqdm import tqdm
 
-def grep(query: str, flags: list[str], pagename = False, fmt = "{}", output = sys.stdout):
-    if flags:
-        vals = [flagmap[item] for item in flags if item in flagmap]
-        flags = 0
-        for f in vals:
-            flags |= f
-    
+def grep(query: str, flags: int = 0, pagename: bool = False, fmt: str = "{}", output = sys.stdout, count: bool = False):    
     # latest.xml should be enwiktionary-YYYYMMDD-pages-meta-current.xml
     context = etree.iterparse("dumps/latest.xml", events=("end",), tag="{*}page")
 
@@ -20,7 +13,10 @@ def grep(query: str, flags: list[str], pagename = False, fmt = "{}", output = sy
     if not pagename:
         exp = re.compile(query, flags = flags)
     
-    if output == sys.stdout:
+    if count:
+        n = 0
+    
+    if output == sys.stdout and not count:
         iterator = context  # don't use tqdm when writing to stdout, it looks stupid
     else:
         iterator = tqdm(context, unit = "ppg", desc = "Searching", dynamic_ncols = True)
@@ -35,12 +31,17 @@ def grep(query: str, flags: list[str], pagename = False, fmt = "{}", output = sy
 
         if not exp.search(text if text else ""):
             continue
-
-        print(fmt.format(title), file = output)
+        
+        if count:
+            n += 1
+        else:
+            print(fmt.format(title), file = output)
 
         elem.clear()
         while elem.getprevious() is not None:
             del elem.getparent()[0]
+    
+    print(n)
 
 if __name__ == "__main__":
     parser = ArgumentParser(
@@ -55,13 +56,16 @@ if __name__ == "__main__":
         help = "regex flags to apply to the query, e.g. 'mi' for multiline and case-insensitive")
 
     parser.add_argument("--pagename", action = "store_true",
-        help = "if passed, {{PAGENAME}} in the query is dynamically substituted for the pagename for each page")
+        help = "if enabled, {{PAGENAME}} in the query is dynamically substituted for the pagename for each page")
 
     parser.add_argument("--format",
         help = "format to output pages in, e.g. '* [[{}]]' to print titles out like a wikitext list")
 
     parser.add_argument("-o", "--output",
         help = "output file (defaults to stdout)")
+    
+    parser.add_argument("-c", "--count", action = "store_true",
+        help = "if enabled, prints out a count of matching pages rather than a list")
     
     args = parser.parse_args()
 
@@ -77,8 +81,13 @@ if __name__ == "__main__":
 
     args.flags = list(args.flags.lower()) if args.flags else ""
 
+    vals = [flagmap[item] for item in args.flags if item in flagmap]
+    flags = 0
+    for f in vals:
+        flags |= f
+
     if args.output:
         with open(args.output, "w", encoding = "utf-8") as file:
-            grep(args.query, args.flags, pagename = args.pagename, fmt = args.format, output = file)
+            grep(args.query, flags, pagename = args.pagename, fmt = args.format, output = file, count = args.count)
     else:
-        grep(args.query, args.flags, pagename = args.pagename, fmt = args.format)
+        grep(args.query, flags, pagename = args.pagename, fmt = args.format, count = args.count)
