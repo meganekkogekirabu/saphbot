c818377329ad8f8e285b4a7a45b30ef6d823c977
Madeleine <gankiann.niu@gmail.com>
2025-09-30 01:08:27 -0400

    langcat_redundant_pagename: change preloading generator count

--- langcat_redundant_pagename.py
+++ langcat_redundant_pagename.py
@@ -10,13 +10,13 @@ site = pywikibot.Site()
 tl_page = pywikibot.Page(site, "Template:langcat")
 
 # run this to generate:
-# dump_grep "{{langcat\|{{PAGENAME}}}}|{{langcat|{\{pagename}}}}" -f i --pagename -o "lists/dumped/langcat_redundant_pagename.txt"
+# python lib/dump_grep.py "{{langcat\|{{PAGENAME}}}}|{{langcat|{\{pagename}}}}" -f i --pagename -o "lists/dumped/langcat_redundant_pagename.txt"
 # should take about ~20 minutes
 gen = pagegenerators.TextIOPageGenerator("lists/dumped/langcat_redundant_pagename.txt")
 
 repl = re.compile("{{langcat\\|{{pagename}}}}", flags = re.I)
 
-for page in pagegenerators.PreloadingGenerator(gen, 10):
+for page in pagegenerators.PreloadingGenerator(gen, 50):
     page.text = repl.sub("{{langcat}}", page.text)
     page.text = page.text.replace("{{langcat|" + page.title() + "}}", "{{langcat}}")
     page.save("Remove redundant pagenames from {{[[Template:langcat|langcat]]}} invocations")
