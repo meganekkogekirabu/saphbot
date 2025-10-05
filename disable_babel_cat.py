8f41e9d08c65482c79dd519248326071861955f7
Madeleine <gankiann.niu@gmail.com>
2025-10-05 11:37:58 -0400

    disable_babel_cat: preload pages; check inactive=1 before calculating timedelta; compile regex once

--- disable_babel_cat.py
+++ disable_babel_cat.py
@@ -3,10 +3,16 @@ Add the `inactive=1` flag to Babel for users whose last contribution was more th
 Discussion: [[Wiktionary:Beer parlour/2025/April#Disabling Babel categorisation for inactive users]]
 """
 
-import re
 from datetime import timedelta
+import re
+import signal
+import sys
 import pywikibot
 from pywikibot.page import BasePage
+from pywikibot.pagegenerators import PreloadingGenerator
+from pywikibot.exceptions import LockedPageError
+
+signal.signal(signal.SIGINT, lambda *_: sys.exit(0))
 
 ignore = []
 
@@ -18,22 +24,26 @@ site = pywikibot.Site()
 tl_page = pywikibot.Page(site, "Template:Babel")
 gen = BasePage(tl_page).getReferences(only_template_inclusion=True, namespaces=2)
 
-for page in gen:
+exp = re.compile(r"({{babel[^}]+)", flags=re.I)
+
+for page in PreloadingGenerator(gen):
     title = page.title()
 
-    if "/" not in title and title not in ignore:
+    if "/" not in title and title not in ignore and "inactive=1" not in page.text:
         last_edit: tuple | None = pywikibot.User(site, title).last_edit
 
         if last_edit is None:
             continue
 
-        delta = (site.server_time() - last_edit[2]) >= timedelta(days=730)
+        try:
+            delta = (site.server_time() - last_edit[2]) >= timedelta(days=730)
 
-        if delta and "inactive=1" not in page.text:
-            page.text = re.sub(
-                r"({{babel[^}]+)", r"\1|inactive=1", page.text, flags=re.I
-            )
-            page.save(
-                "mark users whose last contribution was more than 2 years "
-                + "ago as inactive in Babel"
-            )
+            if delta:
+                page.text = exp.sub(r"\1|inactive=1", page.text)
+                page.save(
+                    "mark users whose last contribution was more than 2 years "
+                    + "ago as inactive in Babel",
+                )
+
+        except LockedPageError:
+            continue
