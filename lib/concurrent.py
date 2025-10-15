3899a2d4bb18c794f3e4e390fd28c966f5b2e25d
Madeleine <gankiann.niu@gmail.com>
2025-10-15 10:38:13 -0400

    lib/concurrent.py: +license notice

--- lib/concurrent.py
+++ lib/concurrent.py
@@ -1,3 +1,23 @@
+"""
+Framework for concurrently processing pages while saving them
+sequentially.
+
+Copyright (c) 2025 Choi Madeleine
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 __all__ = ["ConcurrentBot"]
 
 from concurrent.futures import as_completed, ThreadPoolExecutor
